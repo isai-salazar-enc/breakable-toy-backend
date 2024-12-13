0852c9c74de72f23542cfc19bc817308e2e823f0
Diego Isai Salazar Rico <diego.salazar@ENCEDCSLPLT0742.local>
2024-12-13 16:38:30 -0600

    First commit

---
+++ src/main/java/com/encora/breakable_toy_API/BreakableToyApiApplication.java
@@ -0,0 +1,13 @@
+package com.encora.breakable_toy_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakableToyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakableToyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encora/breakable_toy_API/BreakableToyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.breakable_toy_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakableToyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
