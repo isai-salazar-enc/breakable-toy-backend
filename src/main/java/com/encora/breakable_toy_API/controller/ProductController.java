5a63291088a370cfc4b7f5723303b8b3694a4ce6
Diego Isai Salazar Rico <diego.salazar@encora.com>
2024-12-16 19:19:27 -0600

    feat: add getPaginatedProducts method w/controller & service

--- src/main/java/com/encora/breakable_toy_API/controller/ProductController.java
+++ src/main/java/com/encora/breakable_toy_API/controller/ProductController.java
@@ -1,4 +1,28 @@
 package com.encora.breakable_toy_API.controller;
 
+import com.encora.breakable_toy_API.models.Product;
+import com.encora.breakable_toy_API.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products/{page}")
+    public List<Product> getPaginatedProducts(@PathVariable int page){
+        return productService.getProducts(page);
+    }
 }
--- src/main/java/com/encora/breakable_toy_API/service/ProductService.java
+++ src/main/java/com/encora/breakable_toy_API/service/ProductService.java
@@ -1,4 +1,24 @@
 package com.encora.breakable_toy_API.service;
 
+import com.encora.breakable_toy_API.models.Product;
+import com.encora.breakable_toy_API.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts(int page){
+        return productRepository.findAll(page);
+    }
+
 }
