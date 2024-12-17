9e7720624390be95448fe5741879c28ba2392ac9
Diego Isai Salazar Rico <diego.salazar@encora.com>
2024-12-16 19:08:43 -0600

    feat: add ProductRepository interface and its implementation InMemoryProductRepository - InMemoryProductRepository provides in-memory storage using a HashMap - Add sample data initialization with @PostConstruct - Add findAll method with pagination

---
+++ src/main/java/com/encora/breakable_toy_API/repository/InMemoryProductRepository.java
@@ -0,0 +1,63 @@
+package com.encora.breakable_toy_API.repository;
+
+import com.encora.breakable_toy_API.models.Product;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+// Repository that manages in memory items instead of a database
+@Repository
+public class InMemoryProductRepository implements ProductRepository{
+
+    private final Map<Long, Product> products = new HashMap<>();
+    private static long currentId = 1;
+
+    // Load data in HashMap when API is initialized
+    @PostConstruct
+    public void loadInitialData(){
+        for(int i = 1; i<=30; i++){
+            products.put(currentId++, new Product( (long) i, "Product " + i, 100F, LocalDate.now(), 10, LocalDateTime.now(), LocalDateTime.now()));
+        }
+    }
+
+    // Return every product
+    @Override
+    public List<Product> findAll(int page) {
+        if(page<1){
+            throw new IllegalAccessError("Page value not allowed");
+        }
+
+        int size = 10;
+        List<Product> productList = new ArrayList<Product>(products.values());
+        int fromIndex = (page - 1) * size;
+        int toIndex = Math.min(fromIndex + size, productList.size());
+        if (fromIndex >= productList.size()) {
+            return Collections.emptyList();
+        }
+
+        return productList.subList(fromIndex, toIndex); // Pagination
+    }
+
+    @Override
+    public Product create(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product outOfStock(Long id) {
+        return null;
+    }
+
+    @Override
+    public Product inStock(Long id, int stock) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+}
--- src/main/java/com/encora/breakable_toy_API/repository/ProductRepository.java
+++ src/main/java/com/encora/breakable_toy_API/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package com.encora.breakable_toy_API.repository;
+import com.encora.breakable_toy_API.models.Product;
+import java.util.List;
 
-public class ProductRepository {
+public interface ProductRepository {
+    // Definition of operations
+    List<Product> findAll(int page); // Get products w/pagination of 10 items per page
+    Product create(Product product); // Create new product
+    Product outOfStock(Long id); // Make it unavailable and set stock to 0
+    Product inStock(Long id, int stock); // Update stock
+    Product update(Product product); // Update information of the product
 }
